364a7060bd687dea5ebb73d9e3023e228a5178d4
Kiril <kpanar82@gmail.com>
2019-09-05 17:15:13 +0300

    fix comments

--- HW3/src/js/task2.js
+++ HW3/src/js/task2.js
@@ -1,18 +1,19 @@
-let a = parseFloat(prompt('Enter length of side A'));
-let b = parseFloat(prompt('Enter length of side B'));
+let firstSide = parseFloat(prompt('Enter length of side A'));
+let secondSide = parseFloat(prompt('Enter length of side B'));
 let angle = prompt('Enter angle');
 
-function calc(a,b,angle) {
+function calc(firstSide,secondSide,angle) {
     var y = Math.cos(angle * Math.PI / 180);
-    var c2 = (b*b) + (a*a) - (2*b*a)* y;
+    var c2 = (secondSide*secondSide) + (firstSide*firstSide) - (2*secondSide*firstSide)* y;
     var c = Math.sqrt(c2);
-    var p = c + b + a;
+    var p = c + secondSide + firstSide;
     var pP = p/2;
-    var s2 = pP * (pP-a)*(pP-b)*(pP-c);
+    var s2 = pP * (pP-firstSide)*(pP-secondSide)*(pP-c);
     var s = Math.sqrt(s2);
-    console.log('input data '+a+'(a length), '+b+'(b length), '+angle+'(α- angle):');
-    console.log('C length:'+c+'');
-    console.log('square:'+s+'');
-    console.log('perimeter:'+p+'');
+    console.log(`
+                 input data ${firstSide}(a length), ${secondSide}(b length), ${angle}(α- angle):
+                 C length: ${Math.round(c)}
+                 square:'${Math.round(s)}
+                 perimeter: ${Math.round(p)}`);
 }
-calc(a,b,angle);
+calc(firstSide,secondSide,angle);
